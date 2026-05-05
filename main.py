c4293a97f4b92c6f15b6c8ec656bd3f45e415f58
gunqiu <myu88426@gmail.com>
2026-05-05 10:19:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=8ea2bcb8-abe4-472d-a6bf-4e1f3bc0f04d",
     # 添加更多链接
 ]
 
